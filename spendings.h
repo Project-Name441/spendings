a8aa38635538a3b301aa4a1774719a587425d1e3
Project-Name441 <36046509+Project-Name441@users.noreply.github.com>
2018-03-08 16:49:22 -0800

    Create spendings.h

---
+++ spendings.h
@@ -0,0 +1,20 @@
+**spendings.h file
+
+
+
+
+ifndef SPENDINGS_H
+#define SPENDINGS_H
+#include <vector>
+#include <QObject> //allows usage of signals and slots
+#include <QString>
+using namespace std;
+
+class Spendings: public QObject
+{
+    Q_OBJECT
+protected:
+    Spendings();
+
+private:
+  
